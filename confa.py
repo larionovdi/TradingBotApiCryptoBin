8726396a79326db7c9d5bfd2e43d56f5d3c0a030
larionovdi <101740339+larionovdi@users.noreply.github.com>
2022-06-05 20:54:35 +0300

    Update confa.py

--- confa.py
+++ confa.py
@@ -1,2 +1,2 @@
-API_KEY = 'qymYwEeMiToD6GbpOg7pFmk89yzAJOyaqT8MHcTSbp1C9NZnLpeS2NwqYQHW63Ar'
-API_SECRET = 'FQPl8R60re0ock55AhQvSu1VdJIePknmjtck1QbxMC1XoZJJa45Tw5YuSrjYG1dG'
+API_KEY = 'secret'
+API_SECRET = 'secret'
